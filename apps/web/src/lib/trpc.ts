7567fcb7b1e65307b207e1d843be01767445f955
harryplusplus <harryplusplus@gmail.com>
2026-01-06 08:22:47 +0900

    chore: Add TRPC Client for web

---
+++ apps/web/src/lib/trpc.ts
@@ -0,0 +1,47 @@
+import type { AppRouter } from '@repo/api'
+import {
+  createTRPCClient,
+  httpLink,
+  httpSubscriptionLink,
+  loggerLink,
+  splitLink,
+} from '@trpc/client'
+import { createTRPCContext } from '@trpc/tanstack-react-query'
+
+export const { TRPCProvider, useTRPC, useTRPCClient } =
+  createTRPCContext<AppRouter>()
+
+const TRPC_API_URL = `${process.env.NEXT_PUBLIC_API_URL}/trpc`
+
+export function createTrpcClient() {
+  return createTRPCClient<AppRouter>({
+    links: [
+      loggerLink({
+        enabled: (opts) =>
+          (process.env.NODE_ENV === 'development' &&
+            typeof window !== 'undefined') ||
+          (opts.direction === 'down' && opts.result instanceof Error),
+      }),
+      splitLink({
+        condition: (op) => op.type === 'subscription',
+        true: httpSubscriptionLink({
+          url: TRPC_API_URL,
+          eventSourceOptions: () => {
+            return {
+              withCredentials: true,
+            } satisfies EventSourceInit
+          },
+        }),
+        false: httpLink({
+          url: TRPC_API_URL,
+          fetch: (url, options) => {
+            return fetch(url, {
+              ...options,
+              credentials: 'include',
+            })
+          },
+        }),
+      }),
+    ],
+  })
+}
