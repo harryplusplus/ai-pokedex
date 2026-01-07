f2ef785e3d12cd8e0169970794deb8fc22e32c4a
harryplusplus <harryplusplus@gmail.com>
2026-01-07 14:53:28 +0900

    chore(style): remove unused styles

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -10,13 +10,11 @@ export default function Home() {
             <GoogleSignInButton />
           </div>
         </div>
-        <div className="flex w-1/2 flex-col items-center justify-center gap-6">
-          <div className="mb-8 space-y-2 text-center">
-            <div className="rounded-2xl bg-black/20 px-1 py-1 backdrop-blur-sm">
-              <h1 className="bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 bg-clip-text text-6xl font-bold text-transparent">
-                AI 포켓몬 도감
-              </h1>
-            </div>
+        <div className="flex w-1/2 justify-center">
+          <div className="rounded-2xl bg-black/20 px-1 py-1 backdrop-blur-sm">
+            <h1 className="bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 bg-clip-text text-6xl font-bold text-transparent">
+              AI 포켓몬 도감
+            </h1>
           </div>
         </div>
       </div>
