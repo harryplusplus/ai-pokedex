7de818688aa818bfc2a37dce09625b9715493ec6
harryplusplus <harryplusplus@gmail.com>
2026-01-07 13:15:04 +0900

    fix(style): Size up Google sign in button

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" className="bg-black">
       <body className="overflow-y-scroll antialiased">
         <TrpcQueryProviders>
           <Background />
