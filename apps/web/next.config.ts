73b70e63fa300cbaa085e9d424456405ffa36d0e
harryplusplus <harryplusplus@gmail.com>
2026-01-10 18:52:56 +0900

    chore: Add google image url

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  images: {
+    remotePatterns: [new URL('https://*.googleusercontent.com/**')],
+  },
 }
 
 export default nextConfig
