425855c8660f5dec5ffa0bb11a6bcd0bda4a647d
harryplusplus <harryplusplus@gmail.com>
2026-01-16 22:43:59 +0900

    fix: import auth

--- apps/web/src/app/api/auth/[...all]/route.ts
+++ apps/web/src/app/api/auth/[...all]/route.ts
@@ -1,5 +1,5 @@
 import { toNextJsHandler } from 'better-auth/next-js'
 
-import { auth } from '@/server/lib/auth'
+import { auth } from '@/server/auth'
 
 export const { POST, GET } = toNextJsHandler(auth)
