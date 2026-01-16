a55c7082c57a7153dd392f495b8130999d2d2bf8
harryplusplus <harryplusplus@gmail.com>
2026-01-17 04:34:41 +0900

    chore: Add assign to constant

--- apps/web/src/server/auth.ts
+++ apps/web/src/server/auth.ts
@@ -1,6 +1,9 @@
 import { Config, container, DbService } from '@repo/server'
 import { betterAuth } from 'better-auth'
 
+const GOOGLE_CLIENT_ID =
+  '179009707988-aej3ieths6olvv4amed5asbc2meaubic.apps.googleusercontent.com'
+
 const dbService = await container.resolve(DbService)
 const config = await container.resolve(Config)
 
@@ -9,8 +12,7 @@ export const auth = betterAuth({
   database: dbService.pool,
   socialProviders: {
     google: {
-      clientId:
-        '179009707988-aej3ieths6olvv4amed5asbc2meaubic.apps.googleusercontent.com',
+      clientId: GOOGLE_CLIENT_ID,
       clientSecret: config.envVars.GOOGLE_CLIENT_SECRET,
     },
   },
