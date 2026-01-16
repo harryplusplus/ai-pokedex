6b5b35cd72cc1f581400b1f264bbef6af9d6b23e
harryplusplus <harryplusplus@gmail.com>
2026-01-17 03:07:15 +0900

    chore: Add ts testing log

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,3 +2,5 @@ export { Config } from './config/config.ts'
 export { container } from './container.ts'
 export { DbService } from './db/service.ts'
 export { validateTimeZone } from './utils.ts'
+console.log(__filename)
+console.log(import.meta.filename)
