ff49fc3a80cbfb5fbb96226c50c4f8719126df7e
harryplusplus <harryplusplus@gmail.com>
2026-01-16 14:17:31 +0900

    wpi: server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,12 +1,2 @@
-import { Container } from 'esdi'
-
-import { App } from './app.ts'
-
-export async function main() {
-  const container = new Container()
-
-  const app = await container.resolve(App)
-  await app.check()
-
-  await container.destroy()
-}
+export * from './app.ts'
+export * from 'esdi'
