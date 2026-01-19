e75a7b0be9aa71f6bb9036c06a97c149395ca9fb
harryplusplus <harryplusplus@gmail.com>
2026-01-19 19:41:51 +0900

    chore: Move files

--- packages/pocket-di/src/types/utils.ts
+++ packages/pocket-di/src/types/utils.ts
@@ -4,7 +4,3 @@ export type Any = any
 export type Constructor<T> = new (...args: Any[]) => T
 
 export type MaybePromise<T> = T | Promise<T>
-
-export type DeepReadonly<T> = {
-  readonly [P in keyof T]: DeepReadonly<T[P]>
-}
